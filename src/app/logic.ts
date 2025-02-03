8c8afc3fe72959053e00ea5bb313ccbd1288d614
Neemias Santos <neemiassgc@outlook.com>
2025-02-03 00:48:13 -0300

    chore: remove the letter ç for it not to be included

--- src/app/logic.ts
+++ src/app/logic.ts
@@ -16,12 +16,12 @@ export function generatePassword(charOptions: CharOptions, length: number): stri
   }
   const charTypeSet: CharPool = {
     lowercase: [
-      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-      "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ç"
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
     ],
     uppercase: [
-      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-      "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ç"
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
     ],
     numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     special: ["!", "@", "$", "#", "$", "&", "*", "=", "+", "-", "_", "?", ":", ";"]
